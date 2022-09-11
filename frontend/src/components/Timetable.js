31b480464c0439b5c508328a990a2352e6d6f63b
Roy Arad <86407401+AradRoy@users.noreply.github.com>
2022-09-11 17:40:03 +0300

    Timetable fix with names

--- frontend/src/components/Timetable.js
+++ frontend/src/components/Timetable.js
@@ -1,58 +1,76 @@
-import React from 'react'
-import { ViewState, EditingState, IntegratedEditing } from "@devexpress/dx-react-scheduler"
-import { WeekView, Scheduler, Appointments, AppointmentForm, AppointmentTooltip, Resources } from '@devexpress/dx-react-scheduler-material-ui';
-import { red, lightGreen, blue } from '@mui/material/colors';
+import React from "react";
+import ListGroup from "react-bootstrap/ListGroup";
 
-
+import {
+  ViewState,
+  EditingState,
+  IntegratedEditing,
+} from "@devexpress/dx-react-scheduler";
+import {
+  WeekView,
+  Scheduler,
+  Appointments,
+  AppointmentForm,
+  AppointmentTooltip,
+  Resources,
+} from "@devexpress/dx-react-scheduler-material-ui";
+import { red, lightGreen, blue } from "@mui/material/colors";
 
 function Timetable(props) {
-    const lessondata = props.lessonArray.map((lesson) => {
-        return ({
-            title: lesson.title,
-            startDate: lesson.start_time,
-            endDate: lesson.end_time,
-            coach: lesson.coach,
-            athletes: lesson.athletes,
-        })
-    })
-    return (
-        <>
-            <Scheduler locale='en-IL' data={lessondata} height={700}>
-                <ViewState currentDate="2022-09-03" />
-                <EditingState />
-                <IntegratedEditing />
-                <WeekView
-                    excludedDays={[5, 6]}
-                    startDayHour={11}
-                    endDayHour={24}
-                    cellDuration={60}
-                />
-                <Appointments />
-                <Resources
-                    data={
-                        [
-                            {
-                                id: 'coach',
-                                fieldName: 'coach',
-                                title: 'Coach',
-                                instances: [
-                                    { id: 'Yotam', text: 'Yotam', color: red[500] },
-                                    { id: 'Yonni', text: 'Yonni', color: lightGreen[500] },
-                                    { id: 'Johnny', text: 'Johnny', color: blue[500] }
+  const lessondata = props.lessonArray.map((lesson) => {
+    return {
+      title: lesson.title,
+      startDate: lesson.start_time,
+      endDate: lesson.end_time,
+      coach: lesson.coach,
+      athletes: lesson.athlete_names,
+    };
+  });
+  const Appointment = ({ children, style, ...restOfProps }) => (
+    <Appointments.Appointment
+      {...restOfProps}
+      style={{
+        ...style,
+        backgroundColor: "#FFC187",
+        borderRadius: "8px",
+      }}
+    >
+      {children}
+    </Appointments.Appointment>
+  );
 
-                                ],
-                                allowMultiple: false,
-                                isMain: true
-                            }
-                        ]
-                    }
-                    mainResourceName='coach'
-                />
-                <AppointmentTooltip />
-                <AppointmentForm
-                    readonly />
-            </Scheduler>
-        </>
-    )
+  const Content = ({ children, appointmentData, ...restProps }) => (
+    <AppointmentTooltip.Content
+      {...restProps}
+      appointmentData={appointmentData}
+    >
+      <ListGroup>
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+    </AppointmentTooltip.Content>
+  );
+  return (
+    <>
+      <Scheduler locale="en-IL" data={lessondata} height={700}>
+        <ViewState currentDate="2022-09-03" />
+        <EditingState />
+        <IntegratedEditing />
+        <WeekView
+          excludedDays={[5, 6]}
+          startDayHour={11}
+          endDayHour={24}
+          cellDuration={60}
+        />
+        <Appointments appointmentComponent={Appointment} />
+
+        <AppointmentTooltip contentComponent={Content} />
+        <AppointmentForm readonly />
+      </Scheduler>
+    </>
+  );
 }
-export default Timetable
+export default Timetable;
